a7577357f7856fb9922acf891f1dd2f784625d2d
Emerson Lopes <lupuselit@outlook.com>
2019-06-29 19:25:46 -0300

    add hello world

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// in go if you create a variable you must use it
+func main() {
+  var name string = "emerson"
+  fmt.Println(name)
+
+  var age int = 37
+
+  fmt.Println(age)
+
+}
